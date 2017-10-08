e4f00ef65849c19e421a81945a366e5d4e6ed86c
irvanEstructior <niero.neo@gmail.com>
2017-10-08 22:01:56 +0700

    edit service work

--- scripts/app.js
+++ scripts/app.js
@@ -361,9 +361,9 @@
     app.saveSelectedCities();
   }
 
-  // if ('serviceWorker' in navigator) {
-  //   navigator.serviceWorker
-  //       .register('./service-worker.js')
-  //       .then(function() { console.log('Service Worker Registered'); });
-  // }
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('./pwa_tester/service-worker.js')
+        .then(function() { console.log('Service Worker Registered'); });
+  }
 })();
